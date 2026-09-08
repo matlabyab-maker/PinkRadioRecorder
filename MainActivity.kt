e02961489572865e48ae948a100562aba05418cf
matlabyab-maker <matlabyab@gmail.com>
2026-09-08 04:01:09 +0330

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pinkradio
+import android.app.Activity
+import android.os.Bundle
+class MainActivity: Activity(){
+ override fun onCreate(b:Bundle?){super.onCreate(b)}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+PinkRadioRecorder
+Free internet radio recorder.
+Supports stream URL, m3u, m3u8, background playback and AMR recording.
